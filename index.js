b21b745dcace104a4aafefb83ca68f61bc2dbd5e
ajay2507 <pajay2507@gmail.com>
2019-05-24 00:14:50 -0700

    removed no-use-before-define and add env variable

--- index.js
+++ index.js
@@ -3,5 +3,11 @@ module.exports = {
         './lib/recommended.js',
         './lib/stylistic.js'
     ],
-    rules: {}
+    rules: {},
+    env: {
+        browser: true,
+        node: true,
+        es6: true,
+        mocha: true
+    }
 };
